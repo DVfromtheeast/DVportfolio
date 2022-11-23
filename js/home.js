614c5e26a91491f13341c8bd8b5bb8acba7789ea
DV <dominik4793@gmail.com>
2022-11-23 14:32:59 +0700

    reset default theme

--- js/home.js
+++ js/home.js
@@ -20,20 +20,20 @@ var slider = document.getElementById("slider");
 var illustration = document.getElementById("illustration");
 
 let localData = localStorage.getItem("theme");
-if (localData == "light") {
-  illustration.src = "./images/day.png";
-  document.body.classList.remove("dark-theme");
-} else if (localData == "dark") {
+if (localData == "dark") {
   illustration.src = "./images/night.png";
-  document.body.classList.add("dark-theme");
+  document.body.classList.remove("light-theme");
+} else if (localData == "light") {
+  illustration.src = "./images/day.png";
+  document.body.classList.add("light-theme");
 }
 slider.onclick = function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    illustration.src = "./images/night.png";
-    localStorage.setItem("theme", "dark");
-  } else {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
     illustration.src = "./images/day.png";
     localStorage.setItem("theme", "light");
+  } else {
+    illustration.src = "./images/night.png";
+    localStorage.setItem("theme", "dark");
   }
 };
