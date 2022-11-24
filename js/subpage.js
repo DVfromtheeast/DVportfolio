1ccfbfcc871d693f052bae4658b5d33f0f88e5a0
DV <dominik4793@gmail.com>
2022-11-24 10:25:32 +0700

    Fix theme and navbar in sub pages

--- js/subpage.js
+++ js/subpage.js
@@ -24,20 +24,16 @@ if (localStorage.getItem("theme") == null) {
 }
 let localData = localStorage.getItem("theme");
 if (localData == "dark") {
-  illustration.src = "./images/night.png";
   document.body.classList.remove("light-theme");
 } else if (localData == "light") {
-  illustration.src = "./images/day.png";
   document.body.classList.add("light-theme");
 }
 
 slider.onclick = function () {
   document.body.classList.toggle("light-theme");
   if (document.body.classList.contains("light-theme")) {
-    illustration.src = "./images/day.png";
     localStorage.setItem("theme", "light");
   } else {
-    illustration.src = "./images/night.png";
     localStorage.setItem("theme", "dark");
   }
 };
